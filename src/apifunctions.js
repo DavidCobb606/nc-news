7b386e25094770929fbfe533da8909092a12f06d
DavidCobb606 <Davidcobb606@gmail.com>
2022-11-01 15:09:37 +0000

    task-7 finished

--- src/apifunctions.js
+++ src/apifunctions.js
@@ -21,3 +21,12 @@ export const getArticleById = (article_id) => {
         return res
     })    
 }
+
+export const alterServerVotes = (votes, article_id) => {
+
+    return axios.patch(`https://backend-server-6006.herokuapp.com/api/articles/${article_id}`, {inc_votes: votes })
+    .then((res) => {
+        console.log(res.data.articles.votes)
+        return res
+        })
+}
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,28 +2,42 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getArticleById } from "../apifunctions"
 import ArticleCards from "./ArticleCards"
+import { alterServerVotes } from "../apifunctions"
 
 
 const SingleArticle = () => {
-const [article, setArticle] = useState([])
-const {article_id} = useParams()
-const [isLoading, setIsLoading] = useState(false)
-console.log(article_id)
+
+    const [article, setArticle] = useState([])
+    const {article_id} = useParams()
+    const [isLoading, setIsLoading] = useState(false)
+   
+    const [votes, setVotes] = useState(0)
+
+    useEffect(() => {
+        
+        alterServerVotes(votes, article_id)
+        .then(({data: {articles}}) => {
+           
+            setVotes(articles.votes)
+        })
+        
+    }, [votes])
+  console.log(votes)
 
 useEffect(() => {
 
     getArticleById(article_id)
     .then(({data: {articles}}) => {
-        console.log(articles)
+       
 
         setArticle(articles)
 
     })
     .catch((err) => {
-        console.log(err)
+        
     })
 }, [article_id])
-console.log(article.title)
+
 
 return (
 
@@ -37,11 +51,18 @@ return (
                    <article id="articlecard">
             <p id="articletitle"><b><em>{article.title}</em></b></p>
             <span id="articlebody">{article.body} </span>
-            <p id="topic"> topic: {article.topic} </p>
+            <p id="topic"> Topic: {article.topic} </p>
             <p id="earticleauthor">Posted by {article.author} </p>
             
-
+ <div className="votemiddle"><h2>Votes: {votes}</h2> </div>
+        <button className="vote" id="like" onClick={() => {setVotes(votes+1)}}>Like</button>
+            
+        <button className="vote" id="dislike" onClick={() => {setVotes(votes-1)}}>Dislike </button>
         </article>
+       
+
+       
+  
 
 
             </div>
